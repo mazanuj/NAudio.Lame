bebf47ac8d886ec4b2d94db87080d80b03dbd68a
Oleksandr Mazyaka <mazanuj@gmail.com>
2017-02-27 00:31:02 +0200

    All in one

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -1,5 +1,4 @@
-﻿using NAudio.Lame;
-using NAudio.Wave;
+﻿using TestDll;
 
 namespace TestConsole
 {
@@ -7,23 +6,7 @@ namespace TestConsole
     {
         private static void Main()
         {
-            var tag = new Id3TagData
-            {
-                Title = "A Test File",
-                Artist = "Microsoft",
-                Album = "Windows 7",
-                Year = "2009",
-                Comment = "Test only.",
-                Genre = LameMp3FileWriter.Genres[1],
-                Subtitle = "From the Calligraphy theme",
-                AlbumArt = System.IO.File.ReadAllBytes(@"disco.png")
-            };
-
-            using (var reader = new AudioFileReader(@"test.wav"))
-            using (var writer = new LameMp3FileWriter(@"test.mp3", reader.WaveFormat, 128, tag))
-            {
-                reader.CopyTo(writer);
-            }
+            Converter.ConvertToMp3();
         }
     }
 }
---
+++ TestDll/Converter.cs
@@ -0,0 +1,15 @@
+﻿using NAudio.Lame;
+using NAudio.Wave;
+
+namespace TestDll
+{
+    public class Converter
+    {
+        public static void ConvertToMp3()
+        {
+            using (var reader = new AudioFileReader(@"test.wav"))
+            using (var writer = new LameMp3FileWriter(@"test.mp3", reader.WaveFormat, 128))
+                reader.CopyTo(writer);
+        }
+    }
+}
